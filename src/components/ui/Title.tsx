c71cc6573315fcaefea462d2b6c9df75c6f17d37
Ibis Prevedello <ibiscp@gmail.com>
2024-10-28 13:58:31 -0300

    refactor: Update title.

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -4,13 +4,7 @@ const Title: React.FC = () => {
   return (
     <header className="w-full max-w-md text-white text-center">
       <h1 className="text-3xl font-bold mb-2 flex items-center justify-center">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-2" viewBox="0 0 24 24" fill="white" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z"/>
-        </svg>
-        SatsQueue
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 ml-2" viewBox="0 0 24 24" fill="white" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z"/>
-        </svg>
+        ϟ SatsQueue ϟ
       </h1>
     </header>
   );
